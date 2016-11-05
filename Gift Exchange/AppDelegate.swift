48dff5a2777629607755978f62af2b59dc32988b
Eric Internicola <intere@gmail.com>
2016-11-05 11:57:40 -0600

    Added debug mode to Fabric.

--- Gift Exchange/AppDelegate.swift
+++ Gift Exchange/AppDelegate.swift
@@ -20,7 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        Fabric.with([Crashlytics.self])
+        Fabric.sharedSDK().debug = true
+        Fabric.with([Crashlytics.self()])
         return true
     }
 
